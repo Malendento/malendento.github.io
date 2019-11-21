1f7b448ea7c1b1f76e79e37aaa42f5936fd26f35
Malendento <50326927+Malendento@users.noreply.github.com>
2019-11-21 19:01:33 +0200

    zxc

---
+++ krosno/js/main.js
@@ -0,0 +1,38 @@
+        $(document).on('click', 'a[href^="#"]', function (event) {
+            event.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top - 120
+            }, 900);
+        });
+
+        $(document).ready(function () {
+            $('.sidebar-content a, .city-switch').on('click', function () {
+                $('.sidebar__mobile').removeClass('active-sidebar');
+            });
+
+            $('.sidebar-content a, .city-switch').on('click', function () {
+                $('.hamburger').removeClass('nav__chrested');
+            });
+
+            $('.hamburger').click(function () {
+                $('.sidebar__mobile').toggleClass('active-sidebar');
+            });
+
+            $('.hamburger').click(function () {
+                $(this).toggleClass('nav__chrested');
+            });
+        });
+
+        $(document).ready(function () {
+            var phoneMask = IMask(
+                document.getElementById('recipient-phone'), {
+                    mask: '+{38}(000)000-00-00'
+                });
+        });
+
+const phoneMask = IMask(
+    document.getElementById('your-id'), {
+      mask: '+{00}(000)000-00-00',
+    },
+  );
