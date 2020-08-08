cb04f100619d22674b74134cb4170103e7be33ef
Malendento <50326927+Malendento@users.noreply.github.com>
2020-08-08 14:31:05 +0300

    rising upd

---
+++ Rising wordpress/js/main.js
@@ -0,0 +1,30 @@
+  $(document).ready(function() {
+            $('.home-slider-block').slick({
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: false,
+                pauseOnHover: true,
+                autoplaySpeed: 3000,
+                speed: 1200,
+                centerMode: false,
+                touchMove: true,
+                draggable: true,
+                dots: false,
+                arrows: true,
+                prevArrow: $('.home-slider-left img'),
+                nextArrow: $('.home-slider-right img'),
+                responsive: [{
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                }]
+            });
+        });
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+   document.querySelector('.sidebar').classList.toggle('active-sidebar');
+    document.querySelector('.hamburger').classList.toggle('hamburger-crossed');
+});
